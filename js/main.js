ac36b7dd493ec6e70a373c3130ed1ddd296e4c0f
Al-Junaidi <ziadahm3@gmail.com>
2025-10-15 02:18:14 +0300

    final first deployment

--- js/main.js
+++ js/main.js
@@ -147,3 +147,16 @@
 
 
 }());
+
+document.addEventListener('contextmenu', (e) => e.preventDefault());
+document.onkeydown = (e) => {
+	// Disable F12, Ctrl+Shift+I, Ctrl+Shift+J, Ctrl+U
+	if (
+		event.keyCode === 123 || // F12
+		(e.ctrlKey && e.shiftKey && e.keyCode === 'I'.charCodeAt(0)) || // Ctrl+Shift+I
+		(e.ctrlKey && e.shiftKey && e.keyCode === 'J'.charCodeAt(0)) || // Ctrl+Shift+J
+		(e.ctrlKey && e.keyCode === 'U'.charCodeAt(0)) // Ctrl+U
+	) {
+		return false;
+	}
+};
